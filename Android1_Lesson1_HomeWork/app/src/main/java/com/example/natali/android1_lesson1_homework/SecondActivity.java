03aa3b56bc30462ad59c28ad21d4cc4400360b37
Nataliani80 <As-Terpsixora-312@yandex.ru>
2019-04-22 13:10:57 +0300

    добавила стили и текущую дату и время

--- Android1_Lesson1_HomeWork/app/src/main/java/com/example/natali/android1_lesson1_homework/SecondActivity.java
+++ Android1_Lesson1_HomeWork/app/src/main/java/com/example/natali/android1_lesson1_homework/SecondActivity.java
@@ -5,7 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SecondActivity extends AppCompatActivity {
+
+    public String dataNow() {
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("E dd.MM.yyyy");
+        return formatForDateNow.format(dateNow);
+    }
 
     @Override
     protected void onCreate (Bundle saveInstanceState) {
@@ -14,5 +23,8 @@ public class SecondActivity extends AppCompatActivity {
         String text = getIntent().getExtras().getString("city");
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(text);
+        String data = dataNow();
+        TextView textViewData = (TextView) findViewById(R.id.textViewData);
+        textViewData.setText(data);
     }
 }
