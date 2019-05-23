13e17636f10c44aace05c1297877bae1cc20c67e
Nataliani80 <As-Terpsixora-312@yandex.ru>
2019-05-23 22:56:08 +0300

    сделала Navigation Drawer и цветовую индикацию выбранного пункта

--- Android1_Lesson1_HomeWork/app/src/main/java/com/example/natali/android1/MainActivity.java
+++ Android1_Lesson1_HomeWork/app/src/main/java/com/example/natali/android1/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.natali.android1;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +24,38 @@ public class MainActivity extends AppCompatActivity {
             ft.replace(R.id.fragment_container, new CitiesFragment());
             ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
             ft.commit();
-
+            getSupportFragmentManager().executePendingTransactions();
         }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle barDrawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout, (Toolbar) findViewById(R.id.toolbar), R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(barDrawerToggle);
+        barDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.cities) {
+            Toast.makeText(this, "Cписок городов пуст", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.map) {
+            Toast.makeText(this, "Поиск по карте", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.about_developer) {
+            Toast.makeText(this, "Информация о разработчике", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.feedback) {
+            Toast.makeText(this, "Отзывы и пожелания", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.share) {
+            Toast.makeText(this, "Поделиться", Toast.LENGTH_SHORT).show();
+        }
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
