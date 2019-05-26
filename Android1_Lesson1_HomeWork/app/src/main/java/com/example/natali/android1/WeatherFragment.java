80765294bb354bfb6dc37320dfa7ad066db1a472
Nataliani80 <As-Terpsixora-312@yandex.ru>
2019-05-26 14:17:14 +0300

    добавила датчики давления и приближения в WeatherFragment

--- Android1_Lesson1_HomeWork/app/src/main/java/com/example/natali/android1/WeatherFragment.java
+++ Android1_Lesson1_HomeWork/app/src/main/java/com/example/natali/android1/WeatherFragment.java
@@ -1,5 +1,10 @@
 package com.example.natali.android1;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -17,6 +22,26 @@ import java.util.Date;
 public class WeatherFragment extends Fragment {
     private static final String KEY_POSITION = "Position";
     private static final String KEY_NAME = "CityName";
+    Sensor sensorProximity;
+    Sensor sensorPressure;
+    TextView textProximity;
+    TextView textPressure;
+    SensorManager sensorManager;
+
+    SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor == sensorProximity) {
+                showSensorTemperature(event);
+            } else if (event.sensor == sensorPressure) {
+                showSensorHumidity(event);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
 
     public static WeatherFragment create(int position, String name) {
         WeatherFragment weatherFragment = new WeatherFragment();
@@ -56,6 +81,16 @@ public class WeatherFragment extends Fragment {
         TextView temperatureView = layout.findViewById(R.id.textViewTemperature);
         TextView textViewData = (TextView) layout.findViewById(R.id.textViewData);
 
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+
+        textProximity = layout.findViewById(R.id.sensor_temperature);
+        sensorProximity = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sensorManager.registerListener(listener, sensorProximity, SensorManager.SENSOR_DELAY_NORMAL);
+
+        textPressure = layout.findViewById(R.id.sensor_humidity);
+        sensorPressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        sensorManager.registerListener(listener, sensorPressure, SensorManager.SENSOR_DELAY_NORMAL);
+
         cityNameView.setText(getName());
         weatherView.setText(weather[getPosition()]);
         temperatureView.setText(temperature[getPosition()]);
@@ -83,4 +118,22 @@ public class WeatherFragment extends Fragment {
         ft.commit();
     }
 
+    private void showSensorTemperature(SensorEvent event) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Proximity Sensor value = ").append(event.values[0]).append("\n");
+        textProximity.setText(stringBuilder);
+    }
+
+    private void showSensorHumidity(SensorEvent event) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Pressure Sensor value = ").append(event.values[0]).append("\n");
+        textPressure.setText(stringBuilder);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(listener, sensorProximity);
+        sensorManager.unregisterListener(listener, sensorPressure);
+    }
 }
