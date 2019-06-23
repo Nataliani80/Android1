a415c4d3fa827a601a1187dddb0f39f7e6387855
Nataliani80 <As-Terpsixora-312@yandex.ru>
2019-06-23 22:06:09 +0300

    добавила отображение текущего местоположения пользователя на карте

--- Android1_Lesson1_HomeWork/app/src/main/java/com/example/natali/android1/MapFragment.java
+++ Android1_Lesson1_HomeWork/app/src/main/java/com/example/natali/android1/MapFragment.java
@@ -1,18 +1,37 @@
 package com.example.natali.android1;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private static final int PERMISSION_REQUEST_CODE = 11;
+    private double latitude = 0;
+    private double longitude = 0;
+    private LatLng location = null;
+    private MarkerOptions markerOptions = null;
 
     public static MapFragment newInstance() {
         return new MapFragment();
@@ -30,8 +49,84 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         return layout;
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            requestLocation();
+            map = googleMap;
+            map.setMyLocationEnabled(true);
+            map.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+                @Override
+                public boolean onMyLocationButtonClick() {
+                    if (location != null) {
+                        map.moveCamera(CameraUpdateFactory.newLatLng(location));
+                    }
+                    return true;
+                }
+            });
+        } else {
+            requestLocationPermissions();
+        }
     }
+
+    @SuppressLint("MissingPermission")
+    private void requestLocation() {
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Activity.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        String provider = LocationManager.NETWORK_PROVIDER;
+        locationManager.requestLocationUpdates(provider, 1000, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                MapFragment.this.location = new LatLng(latitude, longitude);
+                if (markerOptions == null) {
+                    markerOptions = new MarkerOptions().visible(false).icon(null).position(MapFragment.this.location);
+                    map.addMarker(markerOptions);
+                } else {
+                    markerOptions.position(MapFragment.this.location);
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        });
+    }
+
+    private void requestLocationPermissions() {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                requestLocation();
+            }
+        }
+    }
+
 }
