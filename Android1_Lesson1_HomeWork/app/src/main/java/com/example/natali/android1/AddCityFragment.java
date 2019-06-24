4871bc0da252c3a31e13533972ac805621e2ddc3
Nataliani80 <As-Terpsixora-312@yandex.ru>
2019-06-24 15:35:52 +0300

    сделала заготовку для экрана добавления города

--- Android1_Lesson1_HomeWork/app/src/main/java/com/example/natali/android1/AddCityFragment.java
+++ Android1_Lesson1_HomeWork/app/src/main/java/com/example/natali/android1/AddCityFragment.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddCityFragment extends Fragment {
 
     private EditText addCity;
+    private RecyclerView recyclerView;
+
 
     public static AddCityFragment create() {
         return new AddCityFragment();
@@ -25,6 +32,22 @@ public class AddCityFragment extends Fragment {
         View layout = inflater.inflate(R.layout.fragment_add_city, container, false);
         addCity = layout.findViewById(R.id.add_city);
 
+        List<City> addCities = new ArrayList<>();
+        City city = new City();
+        city.setId(1);
+        city.setName("Белгород");
+        addCities.add(city);
+        city = new City();
+        city.setId(2);
+        city.setName("Москва");
+        addCities.add(city);
+        city = new City();
+        city.setId(3);
+        city.setName("Воронеж");
+        addCities.add(city);
+
+
+        initRecyclerView(layout, addCities);
         createButton(layout);
         return layout;
     }
@@ -44,5 +67,17 @@ public class AddCityFragment extends Fragment {
         String city = addCity.getText().toString();
         DatabaseHelper db = DatabaseHelper.getInstance(getActivity().getApplicationContext());
         db.addCity(city);
+    }
+
+    public void initRecyclerView(View layout, List<City> addCities) {
+        RecyclerView recyclerView = layout.findViewById(R.id.recycler_view_add_city);
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        AdapterAddCity adapterAddCity = new AdapterAddCity(addCities);
+        recyclerView.setAdapter(adapterAddCity);
+
     }
 }
